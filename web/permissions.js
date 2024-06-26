c749eb8a87db4289266b0b0968dccb263ff4f1e3
joeriddles <joeriddles10@gmail.com>
2024-06-26 16:42:31 -0700

    Handle permissions error

--- web/permissions.js
+++ web/permissions.js
@@ -1,57 +1,76 @@
 async function _requestPermissions() {
-    try {
-        await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-        const midiAccess = await navigator.requestMIDIAccess({ sysex: true, software: true });
-        console.log({ midiAccess})
-        await callDartOnMidiAccess(midiAccess)
-    } catch (error) {
-        // error = new Error(`Failed to get MIDI access for ${window.location.href}`, { cause: error })
-        console.error(error)
-        await callDartOnMidiAccess(error)
+  try {
+    await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+  } catch (e) {
+    if (e.name === "NotFoundError") {
+      // Either there's no camera, no microphone, or both. We'll retry getting
+      // permissions for each device type separately.
+      try {
+        await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
+      } catch (_) {}
+
+      try {
+        await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+      } catch (_) {}
     }
+  }
+
+  try {
+    const midiAccess = await navigator.requestMIDIAccess({
+      sysex: true,
+      software: true,
+    });
+    console.log({ midiAccess });
+    await callDartOnMidiAccess(midiAccess);
+  } catch (error) {
+    // error = new Error(`Failed to get MIDI access for ${window.location.href}`, { cause: error })
+    console.error(error);
+    await callDartOnMidiAccess(error);
+  }
 }
 
 let onMidiAccessRetried = false;
 
 async function callDartOnMidiAccess(midiAccess) {
-    try {
-        window.onMidiAccess(midiAccess)
-    } catch (err) {
-        if (onMidiAccessRetried) {
-            throw err;
-        }
-
-        console.warn(err)
-
-        // Try again in 3 seconds
-        setTimeout(
-            () => {
-                onMidiAccessRetried = true
-                window.onMidiAccess(midiAccess)
-            },
-            3_000,
-        );
+  try {
+    window.onMidiAccess(midiAccess);
+  } catch (err) {
+    if (onMidiAccessRetried) {
+      throw err;
     }
+
+    console.warn(err);
+
+    // Try again in 3 seconds
+    setTimeout(() => {
+      onMidiAccessRetried = true;
+      window.onMidiAccess(midiAccess);
+    }, 3_000);
+  }
 }
 
 let requestMidiAccessRetried = false;
 
 function timeout(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function requestPermissions() {
-    try {
-        await _requestPermissions()
-    } catch (err) {
-        if (requestMidiAccessRetried) {
-            throw err;
-        }
-    
-        console.warn(err)
-    
-        await timeout(3_000)
-        requestMidiAccessRetried = true
-        await _requestPermissions()
+  try {
+    await _requestPermissions();
+  } catch (err) {
+    if (requestMidiAccessRetried) {
+      throw err;
     }
+
+    console.warn(err);
+
+    await timeout(3_000);
+    requestMidiAccessRetried = true;
+    await _requestPermissions();
+  }
 }
