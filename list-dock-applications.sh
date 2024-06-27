54fddb9417a7c67a45e3f26a5a2f9afca390f841
joeriddles <joeriddles10@gmail.com>
2024-06-27 12:00:41 -0700

    Handle spaces in app names in list-dock-applications.sh

--- list-dock-applications.sh
+++ list-dock-applications.sh
@@ -4,8 +4,12 @@
 app_names=$(osascript -e 'tell application "System Events" to get the name of every process whose visible is true')
 
 # Loop through each app name to find its path
-IFS=", " read -r -a apps <<< "$app_names"
+IFS="," read -r -a apps <<< "$app_names"
+
 for app in "${apps[@]}"; do
-  app_path=$(mdfind "kMDItemKind == 'Application' && kMDItemDisplayName == '$app'" | head -n 1)
+  # strip leading and trailing whitespace
+  app=$(echo $app | awk '{$1=$1};1')
+
+  app_path=$(mdfind "kMDItemKind == 'Application' && kMDItemDisplayName == '$app'" | paste -sd "," -)
   echo "$app: $app_path"
 done
