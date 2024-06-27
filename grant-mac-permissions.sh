059ca1376a464528565ed9b5e1257089bbbd9061
joeriddles <joeriddles10@gmail.com>
2024-06-27 00:55:10 -0700

    Fix weird spacing

--- grant-mac-permissions.sh
+++ grant-mac-permissions.sh
@@ -8,7 +8,7 @@ fi
 
 # See https://entonos.com/2023/06/23/how-to-modify-tcc-on-macos/
 # and https://stackoverflow.com/questions/52706542/how-to-get-csreq-of-macos-application-on-command-line/57259004#57259004
-codesign -dr - "$CHROME_PATH"  2>&1 | awk -F ' => ' '/designated/{print $2}' | csreq -r- -b /tmp/csreq.bin 
+codesign -dr - "$CHROME_PATH" 2>&1 | awk -F ' => ' '/designated/{print $2}' | csreq -r- -b /tmp/csreq.bin 
 CSREQ=$(xxd -p /tmp/csreq.bin  | tr -d '\n')
 
 DB_PATH="/Library/Application Support/com.apple.TCC/TCC.db"
