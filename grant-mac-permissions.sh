abfabd3fd1a458cd5937c3013b0dee0ca031588b
joeriddles <joeriddles10@gmail.com>
2024-06-27 00:33:24 -0700

    Fix CHROME_PATH for CSREQ generation

--- grant-mac-permissions.sh
+++ grant-mac-permissions.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -eux
 
-CHROME_PATH='/Applications/Google Chrome for Testing.app'
-if ! [ -f "$CHROME_PATH" ]; then 
-  echo "CHROME_PATH not found: $CHROME_PATH"
-  exit 1
-fi
+npx -y @puppeteer/browsers install chrome@126
+cd chrome/*/*
+CHROME_PATH='./Google Chrome for Testing.app'
+# See https://entonos.com/2023/06/23/how-to-modify-tcc-on-macos/
+# and https://stackoverflow.com/questions/52706542/how-to-get-csreq-of-macos-application-on-command-line/57259004#57259004
+codesign -dr - "$CHROME_PATH"  2>&1 | awk -F ' => ' '/designated/{print $2}' | csreq -r- -b /tmp/csreq.bin 
+CSREQ=$(xxd -p /tmp/csreq.bin  | tr -d '\n')
 
 DB_PATH="/Library/Application Support/com.apple.TCC/TCC.db"
 DB_COLUMNS="service, client, client_type, auth_value, auth_reason, auth_version, csreq, flags"
@@ -19,11 +21,6 @@ AUTH_VERSION=1  # always 1
 MICROPHONE='kTCCServiceMicrophone'
 CAMERA='kTCCServiceCamera'
 SCREEN_CAPTURE='kTCCServiceScreenCapture'
-
-# See https://entonos.com/2023/06/23/how-to-modify-tcc-on-macos/
-# and https://stackoverflow.com/questions/52706542/how-to-get-csreq-of-macos-application-on-command-line/57259004#57259004
-codesign -dr - "$CHROME_PATH"  2>&1 | awk -F ' => ' '/designated/{print $2}' | csreq -r- -b /tmp/csreq.bin 
-CSREQ=$(xxd -p /tmp/csreq.bin  | tr -d '\n')
 
 # echo ".schema access" | sqlite3 "$DB_PATH"
 sudo sqlite3 "$DB_PATH" "INSERT or REPLACE INTO access ($DB_COLUMNS) VALUES ('$MICROPHONE', '$CLIENT', $CLIENT_TYPE, $AUTH_VALUE, $AUTH_REASON, $AUTH_VERSION, '$CSREQ', 0)"
